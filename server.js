c3ec733e0f9020b6275e5a81b9df1eb2873ecc16
Alissa Maximo <alissahiratamaximo@gmail.com>
2024-11-18 23:41:02 -0300

    feat: created URL /api for the server

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const app = express();
 app.listen(3000, () => {
   console.log("Servidor escutando...");
 });
+
+app.get("/api", (req, res) => {
+  res
+    .status(200)
+    .send("Boas vindas à imersão! Você conseguiu navegar em uma página!");
+});
