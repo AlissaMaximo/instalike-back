1a3cab8d1ca0200b5c33bd9da150b60d64a001bf
Alissa Maximo <alissahiratamaximo@gmail.com>
2024-11-20 21:14:47 -0300

    refactor: separated the controller for listing posts

---
+++ src/config/controllers/postsControllers.js
@@ -0,0 +1,6 @@
+export async function listPosts(req, res) {
+  // Calls the function to get all posts
+  const posts = await getAllPosts();
+  // Sends an HTTP answer with status 200 (OK) and the posts in JSON format
+  res.status(200).json(posts);
+}
--- src/config/routes/postsRoutes.js
+++ src/config/routes/postsRoutes.js
@@ -1,15 +1,11 @@
 import express from "express";
+import { listPosts } from "../controllers/postsControllers.js";
 
 const routes = (app) => {
   // Allows the server to interpret requests with bodies in JSON format
   app.use(express.json());
   // Route to get all posts
-  app.get("/posts", async (req, res) => {
-    // Calls the function to get all posts
-    const posts = await getAllPosts();
-    // Sends an HTTP answer with status 200 (OK) and the posts in JSON format
-    res.status(200).json(posts);
-  });
+  app.get("/posts", listPosts);
 };
 
 export default routes;
