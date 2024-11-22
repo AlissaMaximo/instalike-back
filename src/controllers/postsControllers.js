49d859fca88714e9be6cf160a4406516a941dc52
Alissa Maximo <alissahiratamaximo@gmail.com>
2024-11-21 22:09:44 -0300

    feat: uploaded images are renamed to its id

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,4 +1,5 @@
 import { getAllPosts, createPost } from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listPosts(req, res) {
   // Calls the function to get all posts
@@ -24,13 +25,17 @@ export async function publishNewPost(req, res) {
 export async function uploadImage(req, res) {
   const newPost = {
     description: "",
-    imgUrl: req.file.originalName,
+    imgUrl: req.file.originalname,
     alt: "",
   };
 
   try {
     // Calls the function from postsModel.js to create a post
     const createdPost = await createPost(newPost);
+    // Updated image's server address - the image was uploaded into the server and not into the database
+    const updatedImage = `uploads/${createdPost.insertedId}.png`;
+    // Renames the image in the server
+    fs.renameSync(req.file.path, updatedImage);
     // Sends an HTTP answer with status 200 (OK) and the created post in JSON format
     res.status(200).json(createdPost);
   } catch (error) {
